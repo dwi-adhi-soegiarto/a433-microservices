8565cfe70b5be424a207328c578077ad289bca07
dwi-adhi-soegiarto <139864442+dwi-adhi-soegiarto@users.noreply.github.com>
2023-11-24 18:56:52 +0700

    edit

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,6 +6,6 @@ docker images
 # sesuaikan nama dengan format github packages
 docker tag item-app:v1 ghcr.io/dwi-adhi-soegiarto/item-app:v1 
 # login ke github packages
-$ echo $PAT | docker login ghcr.io --username dwi-adhi-soegiarto --password-stdin 
+echo $PAT | docker login ghcr.io --username dwi-adhi-soegiarto --password-stdin 
 #unggah image ke github packages
 docker push ghcr.io/dwi-adhi-soegiarto/item-app:v1
