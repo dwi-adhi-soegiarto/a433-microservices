75bc332fcbf5dadb9c89be8907c8296e1e3b5103
dwi-adhi-soegiarto <xabi.sudjatmiko@gmail.com>
2023-11-30 20:27:54 +0700

    1

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama <username-docker>/karsajobs:latest
+docker build -t ghcr.io/dwi-adhi-soegiarto/karsajobs:latest .
+
+# Perintah untuk login ke GitHub Package
+echo $PAT | docker login ghcr.io -u dwi-adhi-soegiarto --password-stdin
+
+# Perintah untuk push image ke GitHub Package
+docker push ghcr.io/dwi-adhi-soegiarto/karsajobs:latest
